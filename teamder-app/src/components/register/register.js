2a86cb4fbab84b28a934aaaf9a864fda411510c4
Fed0d <loldffgf@yandex.ru>
2024-01-22 22:26:38 +0300

    Исправленный валидатор

--- teamder-app/src/components/register/register.js
+++ teamder-app/src/components/register/register.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import validator from 'validator';
 import { DOMEN_SERVER, DOMEN_SITE } from '../../config/const';
 import {MainContainer} from "../MainContainer";
 import {Input} from "../Input";
@@ -10,26 +9,23 @@ import {Heading} from "../Heading";
 import {useForm} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup/dist/yup";
 import * as yup from "yup";
-import YupPassword from "yup-password";
 import {useNavigate} from "react-router-dom";
-
-YupPassword(yup);
 
 const schema = yup.object().shape({
     username: yup
         .string()
-        .required('Enter your username'),
+        .required('Логин обязателен для заполнения'),
     password: yup
         .string()
-        .password()
-        .required('Enter the password'),
+        .min(4, 'Пароль должен содержать минимум 4 символа')
+        .required('Пароль обязателен для заполнения'),
     password2: yup
         .string()
-        .oneOf([yup.ref('password')], 'Passwords don\'t match')
-        .required('Repeat the password'),
+        .oneOf([yup.ref('password')], 'Пароли не совпадают')
+        .required('Повторите пароль'),
 });
 
-export default function Register () {
+export default function Register() {
     const navigate = useNavigate();
     const {register, handleSubmit, formState: {errors}} = useForm({
         mode: 'onBlur',
@@ -57,10 +53,8 @@ export default function Register () {
     const submitCheckIn = event => {
 
         event.preventDefault();
-        if(!reg.username) {
-        } else if(reg.password !== reg.password2) {
-        } else if(!validator.isStrongPassword(reg.password, {minSymbols: 0})) {
-        } else {
+        console.log(schema.isValid(reg))
+        if (schema.isValid(reg)) {
             axios.post(DOMEN_SERVER + "/users", {
                 username: reg.username,
                 password: reg.password,
@@ -69,50 +63,50 @@ export default function Register () {
                     alert("Добро пожаловать, " + reg.username + "!")
                     navigate('/mainPage')
                 } else {
-                    alert("There is already a user with this nickname")
+                    alert("Этот никнейм занят :(")
                 }
             }).catch(() => {
-                alert("An error occurred on the server")
+                alert("Какие-то беды :(")
             })
         }
     }
-    return(<MainContainer>
-        <Heading>Register user:</Heading>
-        <Form onSubmit={submitCheckIn}>
-            <Input
-                {...register('username')}
-                type="username"
-                id="username"
-                name="username"
-                value={reg.username}
-                onChange={changeInputRegister}
-                label="Login"
-                error={!!errors.username}
-                helperText={errors?.username?.message}
-            />
-            <Input
-                {...register('password')}
-                type="password"
-                id="password"
-                name="password"
-                value={reg.password}
-                label="Password"
-                onChange={changeInputRegister}
-                error={!!errors.password}
-                helperText={errors?.password?.message}
-            />
-            <Input
-                {...register('password2')}
-                type="password"
-                id="password2"
-                name="password2"
-                value={reg.password2}
-                label="Repeat password"
-                onChange={changeInputRegister}
-                error={!!errors.password2}
-                helperText={errors?.password2?.message}
-            />
-            <PrimaryButton type="submit">Register</PrimaryButton>
-        </Form>
-    </MainContainer>);
-}
+        return (<MainContainer>
+            <Heading>Register user:</Heading>
+            <Form onSubmit={submitCheckIn}>
+                <Input
+                    {...register('username')}
+                    type="username"
+                    id="username"
+                    name="username"
+                    value={reg.username}
+                    onChange={changeInputRegister}
+                    label="Login"
+                    error={!!errors.username}
+                    helperText={errors?.username?.message}
+                />
+                <Input
+                    {...register('password')}
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={reg.password}
+                    label="Password"
+                    onChange={changeInputRegister}
+                    error={!!errors.password}
+                    helperText={errors?.password?.message}
+                />
+                <Input
+                    {...register('password2')}
+                    type="password"
+                    id="password2"
+                    name="password2"
+                    value={reg.password2}
+                    label="Repeat password"
+                    onChange={changeInputRegister}
+                    error={!!errors.password2}
+                    helperText={errors?.password2?.message}
+                />
+                <PrimaryButton type="submit">Register</PrimaryButton>
+            </Form>
+        </MainContainer>);
+    }
